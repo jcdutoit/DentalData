5b4221584bfd207776270a541d3460d52cc1680b
jcdutoit <josephdutoit5@gmail.com>
2021-09-22 14:14:02 -0600

    Added kmeans

--- data_utils.py
+++ data_utils.py
@@ -1,29 +1,46 @@
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
 
 def clean_data(df):
-    
     # Get rid of extra columns
     df.drop(df.columns[3:len(df.columns)], axis=1, inplace=True)
     
     # There are some empty cells in the PayDates.
-    
     df['StartDate'] = pd.to_datetime(df['PayDate'].loc[0])
     df['PayDate'] = df['PayDate'].ffill()
-    # df['PayDate'] = (df['PayDate'])
     df['PayDate'] = pd.arrays.DatetimeArray(pd.to_datetime(df['PayDate'], utc=True))
     
     df['ElapsedDays'] = (df['PayDate'] - df['StartDate']).dt.days.astype(int)
     df.dropna(subset=['PayAmt'], inplace=True)
     df.drop(df.loc[df['PayAmt']=="Null"].index, inplace=True)
-    # df.to_csv('test.csv')
 
     return df
 
+def get_data_for_user(df, pat_num):
+    return df.loc[df['PatNum'] == pat_num]
 
+def plot_date_price(df):
+    plt.scatter(x =df['ElapsedDays'].to_list(), y = df['PayAmt'].astype(float).to_list())        
+    plt.xlim(0,1000)
+    plt.ylim(0,2000)
+    plt.show()
+
+def plot_pat_payments(df, pat_num):
+
+    pat_data = get_data_for_user(df,pat_num)
+    plt.scatter(x=pat_data['ElapsedDays'], y = pat_data['PayAmt'].astype(float))
+    plt.xlim(0,1000)
+    plt.ylim(0,2000)
+    plt.show()
+
+def split_data(df):
+    sep = np.floor(df.)
+    return df.loc[:,:sep], df.loc[:,sep:]
 
 if(__name__ == "__main__"):
-    df = pd.read_csv('./data/Interview_data.csv')
-    df = clean_data(df)
-    print(df.head())
+    df = clean_data(pd.read_csv('data/Interview_data.csv'))
+    plot_date_price(df)
     
+    for i in range(400):
+        plot_pat_payments(df, i)
--- visualize_data.py
+++ visualize_data.py
@@ -1,17 +1,25 @@
 import pandas as pd
-import numpy as np
 from matplotlib import pyplot as plt
 import data_utils
 
 
 def plot_date_price(df):
-    print(df['PayAmt'].astype(float).to_list())
-    plt.scatter(x =df['ElapsedDays'].to_list(), y = df['PayAmt'].astype(float).to_list())
+    plt.scatter(x =df['ElapsedDays'].to_list(), y = df['PayAmt'].astype(float).to_list())        
     plt.xlim(0,1000)
-    plt.ylim(0.10000)
+    plt.ylim(0,2000)
     plt.show()
 
-
+def plot_pat_payments(df, pat_num):
+    pat_data = data_utils.get_data_for_user(df,pat_num)
+    plt.scatter(x=pat_data['ElapsedDays'].to_list(), y = pat_data['PayAmt'].astype(float).to_list())
+    plt.xlim(0,1000)
+    plt.ylim(0,2000)
+    plt.show()
+    
 if(__name__ == "__main__"):
     df = data_utils.clean_data(pd.read_csv('data/Interview_data.csv'))
     plot_date_price(df)
+
+
+    for i in range(400):
+        plot_pat_payments(df, i)
